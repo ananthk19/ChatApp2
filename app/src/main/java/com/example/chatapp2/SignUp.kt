65464f947a8e15ec87b923f040dc3a4c6bb3b9c4
Anand <ananthakrishnan2019@iiitkottayam.ac.in>
2022-08-09 17:22:59 +0530

    Authentication

---
+++ app/src/main/java/com/example/chatapp2/SignUp.kt
@@ -0,0 +1,67 @@
+package com.example.chatapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class SignUp : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail : EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        mAuth = Firebase.auth
+        database = Firebase.database.reference
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+//            var name = edtName.text.toString()
+
+            signUp(email, password)
+        }
+    }
+
+    fun signUp(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(baseContext, "Authentication Success",
+                        Toast.LENGTH_SHORT).show()
+                    writeNewUser(mAuth.currentUser?.uid!!, edtName.text.toString(), email)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun writeNewUser(userId: String, name: String, email: String) {
+        val user = User(name, email)
+        database.child("users").child(userId).setValue(user)
+        Toast.makeText(baseContext, "Inside writeUser",
+            Toast.LENGTH_SHORT).show()
+    }
+
+}
